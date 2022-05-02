308251bb16adf64b3c02f9ff9ae6b8b443e9bd43
alvaroun <alvaromachadoferreira@hotmail.com>
2022-05-02 16:37:25 -0300

    criando material table e  módulo do app material

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
@@ -14,8 +14,7 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule,
-    MatCardModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
