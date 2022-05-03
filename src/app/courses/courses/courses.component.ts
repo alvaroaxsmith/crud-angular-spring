f456e9a3befba5903f4485d2274d70932d98f9b8
alvaroun <alvaromachadoferreira@hotmail.com>
2022-05-03 13:24:40 -0300

    criando spinner da lista de cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -10,14 +10,14 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   displayedColumns = ['name','category'];
 
 
 
   constructor(private coursesService: CoursesService) {
 
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 
   ngOnInit(): void {
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Course } from '../model/course';
-import { tap } from 'rxjs';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +16,8 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
+      first(),
+      delay(5000),
       tap(courses => console.log(courses))
     );
   }
